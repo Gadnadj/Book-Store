f92e47248f62f6a22f559c89e4ca26333e5f2a81
Gad Nadjar <gadnadjar@gmail.com>
2024-12-20 12:00:04 +0200

    add css

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import { HiMiniBars3CenterLeft } from "react-icons/hi2";
+import { IoSearchOutline } from "react-icons/io5";
+import { HiOutlineUser } from "react-icons/hi";
+import { HiOutlineHeart } from "react-icons/hi";
+import { HiOutlineShoppingCart } from "react-icons/hi";
 
 const Navbar = () => {
-  return <div>This is my Navbar</div>;
+  return (
+    <header className="max-w-screen-2xl mx-auto px-4 py-6">
+      <nav className="flex justify-between items-center">
+        {/* left side */}
+        <div className="flex items-center md:gap-16 gap-4">
+          <Link to="/">
+            <HiMiniBars3CenterLeft className="size-6" />
+          </Link>
+
+          {/* search input */}
+          <div className="relative sm:w-72 w-40 space-x-2">
+            <IoSearchOutline className="absolute inline-block left-3 inset-y-2" />
+            <input
+              type="text"
+              placeholder="Search Here"
+              className="bg-[#EAEAEA] w-full py-1 md:px-8 px-6 rounded-md focus:outline-none"
+            />
+          </div>
+        </div>
+
+        {/* right side */}
+        <div>
+          <HiOutlineUser className="size-6" />
+          <button className="hidden sm:block">
+            <HiOutlineHeart className="size-6" />
+          </button>
+
+          <Link to='cart' className="bg-primary p-1 sm:px-6 px-2 flex item-center rounded-md">
+            <HiOutlineShoppingCart className="size-6" />
+            <span>0</span>
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
