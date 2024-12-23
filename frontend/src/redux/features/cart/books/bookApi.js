9fdae9e4cc4b7428eef36309af6c69259082e654
Gad Nadjar <gadnadjar@gmail.com>
2024-12-23 17:02:21 +0200

    add singlebook view

--- frontend/src/redux/features/cart/books/bookApi.js
+++ frontend/src/redux/features/cart/books/bookApi.js
@@ -9,10 +9,9 @@ const baseQuery = fetchBaseQuery({
     if (token) {
       Headers.set("Authorization", `Bearer ${token}`);
     }
-    
+
     return Headers;
   },
-  
 });
 
 const booksApi = createApi({
@@ -24,8 +23,44 @@ const booksApi = createApi({
       query: () => "/",
       providesTags: ["Books"],
     }),
+    fetchBookById: builder.query({
+      query: (id) => `/${id}`,
+      providesTags: (results, error, id) => [{ type: "books", id }],
+    }),
+    addBook: builder.mutation({
+      query: (newBook) => ({
+        url: `/create-book`,
+        method: "POST",
+        body: newBook,
+      }),
+      invalidatesTags: ["Books"],
+    }),
+    updateBook: builder.mutation({
+      query: ({ id, ...rest }) => ({
+        url: `/edit/${id}`,
+        method: "PUT",
+        body: rest,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }),
+      invalidatesTags: ["Books"],
+    }),
+    deleteBook: builder.mutation({
+      query: (id) => ({
+        url: `/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["Books"],
+    }),
   }),
 });
 
-export const { useFetchAllBooksQuery } = booksApi;
+export const {
+  useFetchAllBooksQuery,
+  useFetchBookByIdQuery,
+  useAddBookMutation,
+  useUpdateBookMutation,
+  useDeleteBookMutation,
+} = booksApi;
 export default booksApi;
