cad4c2f5edae8c54ecf310675fc03d08cd9dd5a1
Gad Nadjar <gadnadjar@gmail.com>
2024-12-23 12:09:53 +0200

    add express

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const app = express();
+
+const mongoose = require("mongoose");
+
 const port = process.env.PORT || 3000;
 
-//routes
-app.use("/", (req, res) => {
-  res.send("Welcome to my server!");
-});
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/test");
+  app.use("/", (req, res) => {
+    res.send("Welcome to my server!");
+  });
+}
+
+main()
+  .then(() => console.log("Mongodb Connect successfully!"))
+  .catch((err) => console.log(err));
 
 app.listen(port, () => {
   console.log(`Exemple app listening on port ${port}`);
