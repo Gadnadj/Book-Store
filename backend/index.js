303f450116c70ee6741b74c3504b550f2c9c0038
Gad Nadjar <gadnadjar@gmail.com>
2024-12-23 12:03:59 +0200

    add server

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+//routes
+app.use("/", (req, res) => {
+  res.send("Welcome to my server!");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Exemple app listening on port ${port}`);
 });
