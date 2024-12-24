9eff921e9972ede4a68e836d37d725d84812e264
Gad Nadjar <gadnadjar@gmail.com>
2024-12-24 11:58:03 +0200

    modify register

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -58,7 +58,7 @@ const Register = () => {
           )}
           <div>
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded focus:outline-none">
-              Login
+              Register
             </button>
           </div>
         </form>
