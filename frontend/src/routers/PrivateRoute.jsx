903229b07420e4c524a23f75a92edc29f4806033
Gad Nadjar <gadnadjar@gmail.com>
2024-12-24 13:54:30 +0200

    fix private route

--- frontend/src/routers/PrivateRoute.jsx
+++ frontend/src/routers/PrivateRoute.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { userAuth } from "../context/AuthContext";
-import { Navigate } from "react";
+import { useAuth } from "../context/AuthContext";
+import { Navigate } from "react-router-dom";
 
 const PrivateRoute = ({ children }) => {
-  const { currentUser } = userAuth();
+  const { currentUser } = useAuth();
   if (currentUser) {
     return children;
   }
-  return <Navigate to="login" replace />;
+  return <Navigate to="/login" replace />;
 };
 PrivateRoute.propTypes = {
   children: PropTypes.node.isRequired,
