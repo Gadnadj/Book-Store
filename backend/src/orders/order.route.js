10f9f0fe7c840a479472c7221b93d7650c0bb515
Gad Nadjar <gadnadjar@gmail.com>
2024-12-24 14:34:21 +0200

    fix all bugs

--- backend/src/orders/order.route.js
+++ backend/src/orders/order.route.js
@@ -1,10 +1,10 @@
 const express = require("express");
 
+const { createAOrder } = require("./order.controller");
+
 const router = express.Router();
 
 //create order
-router.post('/', createAOrder)
-
-
+router.post("/", createAOrder);
 
 module.exports = router;
